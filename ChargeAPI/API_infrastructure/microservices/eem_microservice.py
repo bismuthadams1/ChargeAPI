753158614e056034810a8b0d625ea80a0309613f
bismuthadams1 <bismuthadams@gmail.com>
2023-12-11 17:02:09 +0000

    pseudo code from eem microservice

--- ChargeAPI/API_infrastructure/microservices/eem_microservice.py
+++ ChargeAPI/API_infrastructure/microservices/eem_microservice.py
@@ -1,15 +1,26 @@
-# from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
+from ChargeAPI.charge_models.eem_model import EEM_model
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/calculate_charge', methods=['POST'])
-# def calculate_charge():
-#     json_data = request.get_json()
-#     conformer_mol = json_data['conformer_mol']
+eem_model = EEM_model()
 
-#     # Initialize the EEM model and perform calculations
-#     # EEM_Model initialization and calculation logic here...
 
-#     # Return the calculated charges or result
-#     return jsonify({'charge_result': charges})
-...
+@app.route('/calculate_charge', methods=['POST'])
+def calculate_charge():
+    json_data = request.get_json()
+    conformer_mol = json_data['conformer_mol']
+
+    # Initialize the EEM model and perform calculations
+    # EEM_Model initialization and calculation logic here...
+
+    charges = eem_model(conformer_mol)
+
+    # Return the calculated charges or result
+    return jsonify({'charge_result': charges})
+
+
+if __name__ == "__main__":
+    ...
+
+

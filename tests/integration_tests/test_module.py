633f3aa9d4495fe77f91d93311e1c42d02be63c8
vboxuser <bismuthadams@gmail.com>
2024-01-26 15:50:07 +0000

    env fixes and approx tests

--- tests/integration_tests/test_module.py
+++ tests/integration_tests/test_module.py
@@ -1,7 +1,33 @@
 import pytest
+from pytest import approx
+from _pytest.python_api import ApproxMapping
 from ChargeAPI.API_infrastructure import module_version
+from collections.abc import Mapping
 import json
 import os
+
+def nested_approx(expected, rel=None, abs=None, nan_ok=False):
+    if isinstance(expected, Mapping):
+        return ApproxNestedMapping(expected, rel, abs, nan_ok)
+    return pytest.approx(expected, rel, abs, nan_ok)
+
+
+class ApproxNestedMapping(ApproxMapping):
+    def _yield_comparisons(self, actual):
+        for k in self.expected.keys():
+            if isinstance(actual[k], type(self.expected)):
+                gen = ApproxNestedMapping(
+                    self.expected[k], rel=self.rel, abs=self.abs, nan_ok=self.nan_ok
+                )._yield_comparisons(actual[k])
+                for el in gen:
+                    yield el
+            else:
+                yield actual[k], self.expected[k]
+
+    def _check_type(self):
+        for key, value in self.expected.items():
+            if not isinstance(value, type(self.expected)):
+                super()._check_type()
 
 class TestSingle:
     mol  = '\n     RDKit          3D\n\n  3  2  0  0  0  0  0  0  0  0999 V2000\n   -0.7890   -0.1982   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.0061    0.3917   -0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n    0.7951   -0.1936    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0\n  2  3  1  0\nM  END\n'
@@ -18,14 +44,14 @@ class TestSingle:
         json_result = module_version.handle_charge_request(charge_model = 'MBIS', 
                                                            conformer_mol = self.mol,
                                                            batched=False)
-        assert json.loads(json_result['charge_result']) == expected_charges
+        assert json.loads(json_result['charge_result']) == approx(expected_charges)
 
     def test_mbis_charges(self):    
         expected_charges = [0.4147946834564209, -0.8295893669128418, 0.4147946834564209]
         json_result = module_version.handle_charge_request(charge_model = 'MBIS_CHARGE', 
                                                             conformer_mol = self.mol,
                                                             batched=False)
-        assert json.loads(json_result['charge_result']) == expected_charges
+        assert json.loads(json_result['charge_result']) == approx(expected_charges)
 
 class TestBatched:
     mol_file_path = os.path.abspath('./tests/data/mol_file.mol')
@@ -51,18 +77,18 @@ class TestBatched:
         json_result = module_version.handle_charge_request(charge_model='EEM',
                                                            conformer_mol = self.mol_file_path,
                                                            batched=True)
-        assert unpack_charge_file(json_result['charge_result']) ==  read_data_file(EEM_expected_data)
+        assert unpack_charge_file(json_result['charge_result']) == nested_approx(read_data_file(EEM_expected_data))
 
     def test_MBIS_batched(self, unpack_charge_file, read_data_file):
         EEM_expected_data = os.path.abspath('./tests/data/MBIS_batched.json')
         json_result = module_version.handle_charge_request(charge_model='MBIS',
                                                            conformer_mol = self.mol_file_path,
                                                            batched=True)
-        assert  unpack_charge_file(json_result['charge_result']) ==  read_data_file(EEM_expected_data)
+        assert  unpack_charge_file(json_result['charge_result']) ==  nested_approx(read_data_file(EEM_expected_data))
 
     def test_MBIS_CHARGE_batched(self, unpack_charge_file, read_data_file):
         EEM_expected_data = os.path.abspath('./tests/data/MBIS_CHARGE_batched.json')
         json_result = module_version.handle_charge_request(charge_model='MBIS_CHARGE',
                                                            conformer_mol = self.mol_file_path,
                                                            batched=True)
-        assert unpack_charge_file(json_result['charge_result']) == read_data_file(EEM_expected_data)
+        assert unpack_charge_file(json_result['charge_result']) == nested_approx(read_data_file(EEM_expected_data))

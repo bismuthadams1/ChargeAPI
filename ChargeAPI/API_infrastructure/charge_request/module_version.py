295ebd0fc361f220c3f5b2f3176861eae56dd14c
bismuthadams1 <bismuthadams@gmail.com>
2024-09-24 14:19:38 +0100

    new charge models

--- ChargeAPI/API_infrastructure/charge_request/module_version.py
+++ ChargeAPI/API_infrastructure/charge_request/module_version.py
@@ -38,6 +38,34 @@ def handle_charge_request(charge_model: str, conformer_mol: str, batched: bool =
             )
             charge_result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             return prepare_json_outs(charge_result)
+    elif charge_model == 'MBIS_WB_GAS_CHARGE':
+           script_path = f'{os.path.dirname(ChargeAPI.__file__)}/charge_models/mbis_wb_gas_model_charges.py'
+           cmd = (
+                f"conda run -n naglmbis python {script_path} --conformer '{conformer_mol}'  {batched}"
+            )
+           charge_result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+           return prepare_json_outs(charge_result)
+    elif charge_model == 'MBIS_WB_GAS_CHARGE_DIPOLE':
+           script_path = f'{os.path.dirname(ChargeAPI.__file__)}/charge_models/mbis_wb_gas_model_charges_dipole.py'
+           cmd = (
+                f"conda run -n naglmbis python {script_path} --conformer '{conformer_mol}'  {batched}"
+            )
+           charge_result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+           return prepare_json_outs(charge_result)
+    elif charge_model == 'MBIS_WB_WATER_CHARGE':
+           script_path = f'{os.path.dirname(ChargeAPI.__file__)}/charge_models/mbis_wb_water_model_charges.py'
+           cmd = (
+                f"conda run -n naglmbis python {script_path} --conformer '{conformer_mol}'  {batched}"
+            )
+           charge_result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+           return prepare_json_outs(charge_result)
+    elif charge_model == 'MBIS_WB_WATER_CHARGE_DIPOLE':
+           script_path = f'{os.path.dirname(ChargeAPI.__file__)}/charge_models/mbis_wb_water_model_charges_dipole.py'
+           cmd = (
+                f"conda run -n naglmbis python {script_path} --conformer '{conformer_mol}'  {batched}"
+            )
+           charge_result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+           return prepare_json_outs(charge_result)
     else:
             raise NameError
 
@@ -56,15 +84,17 @@ def prepare_json_outs(charge_result: subprocess.CompletedProcess) -> json:
         'error': charge_result.stderr.decode()  # Include the error message if any
     }
     logging.info(json_response)
-    # Return the charge result as a list and the JSON response        
-    return json_response
+    # Return the charge result as a list and the JSON response    
+    return json_response    
 
 
 def main():
     mol = '\n     RDKit          3D\n\n  3  2  0  0  0  0  0  0  0  0999 V2000\n   -0.7890   -0.1982   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.0061    0.3917   -0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n    0.7951   -0.1936    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0\n  2  3  1  0\nM  END\n'
-    json_result = handle_charge_request(charge_model = 'MBIS', 
-                                        conformer_mol = mol,
-                                        batched=False)
+    json_result = handle_charge_request(
+        charge_model = 'MBIS_WB_GAS_CHARGE', 
+        conformer_mol = mol,
+        batched=False
+    )
     print(json_result)
 
 if __name__ == '__main__':
---
+++ ChargeAPI/charge_models/mbis_wb_water_model_charges.py
@@ -0,0 +1,76 @@
+"""
+Requires Nagl MBIS model environment : https://github.com/jthorton/nagl-mbis
+"""
+
+
+import os
+# Check if the module is imported for environment checking
+if os.environ.get("IMPORT_CHECK") == "1":
+    ENV_NAME = "naglmbis"  # Only define the environment name
+    # Stop further execution of the module
+    MODULE = "MBIS_Model_charge"
+else:
+    #used for execution
+    from naglmbis.models import load_charge_model
+    from base_class  import ExternalChargeModel
+    from openff.toolkit.topology import Molecule
+    import rdkit
+    import subprocess
+    import argparse
+
+    class MBIS_Model_charge(ExternalChargeModel):
+
+        _name = "naglmbis_wb_water_charge"
+        def __init__(self, ftype="json"):
+            super().__init__()
+            self.file_type = ftype
+            self.charge_model = load_charge_model(charge_model="nagl-water-charge-wb")
+
+        def __call__(self,  conformer_mol: str, batched: bool, file_method: bool = False) -> list[int] | None:
+            """Get charges for molecule.
+
+            Parameters
+            ----------
+            mapped_smiles: mapped smiles 
+                Molecule to run charge calculation on
+            conformer_mol: str
+                conformer in mol format
+            file_type: str
+                Type of file to output charges to [default = json]
+            file_method: bool
+                Some charge models require temporary files to be written and read, others use python objects stored in internal memory
+            Returns
+            -------
+            charge_files: List of str
+                list containing charges for the molecule or filepath to charges for each conformer
+            """
+            
+            return super().__call__(conformer_mol = conformer_mol, batched = batched)
+
+        def convert_to_charge_format(self, conformer_mol: str):
+
+            rdkit_conformer = rdkit.Chem.rdmolfiles.MolFromMolBlock(conformer_mol, removeHs = False)
+            return rdkit_conformer
+        
+        def assign_charges(self, rdkit_conformer: Molecule):
+
+            charges = self.charge_model.compute_properties(rdkit_conformer)["mbis-charges"]
+            charges = charges.flatten().tolist()
+            return charges
+
+
+if __name__ == "__main__":
+    # Define argparse setup for command line execution
+    parser = argparse.ArgumentParser(description='MBIS charge model arguments')
+    parser.add_argument('--conformer', type=str, help='Conformer mol')
+    parser.add_argument('--batched', help='Batch charges or not', action='store_true')
+    parser.add_argument('--not_batched', help='Batch charges or not', dest='batched', action='store_false')
+    parser.set_defaults(batched = False)
+
+    args = parser.parse_args()
+
+    mbis_model = MBIS_Model_charge()
+    charges = mbis_model(conformer_mol = args.conformer, batched = args.batched) 
+    #ESSENTIAL TO PRINT THE CHARGES TO STDOUT~~~~
+    print(charges)
+    #ESSENTIAL TO PRINT THE CHARGES TO STDOUT~~~~

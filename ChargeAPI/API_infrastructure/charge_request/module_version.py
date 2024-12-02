1dda03bd162899acab08fec5647d2dd9d70f1c3f
bismuthadams1 <bismuthadams@gmail.com>
2024-12-02 11:30:02 +0000

    esp errors

--- ChargeAPI/API_infrastructure/charge_request/module_version.py
+++ ChargeAPI/API_infrastructure/charge_request/module_version.py
@@ -119,7 +119,7 @@ def prepare_json_outs(charge_result: subprocess.CompletedProcess) -> json:
 def main():
     mol = '\n     RDKit          3D\n\n  3  2  0  0  0  0  0  0  0  0999 V2000\n   -0.7890   -0.1982   -0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.0061    0.3917   -0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n    0.7951   -0.1936    0.0000 H   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0\n  2  3  1  0\nM  END\n'
     json_result = handle_charge_request(
-        charge_model = 'MBIS_WB_GAS_ESP_15A', 
+        charge_model = 'MBIS_WB_GAS_ESP_DEFAULT', 
         conformer_mol = mol,
         batched=False
     )
